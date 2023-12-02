52eac16ea3d106268241b8f7f942eededd0b7681
nathanael bale <aymarbale@gmail.com>
2023-12-02 05:18:47 -0500

    timer and sound

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
-let element; let startButton; let text
+let element; let startButton; let text; let buttonSound; let Scoretimer
+let score; let sec = 1;
 (function (window, document, undefined) {
 
     // code that should be taken care of right away
@@ -10,16 +11,27 @@ let element; let startButton; let text
         element = document.getElementById('mainTitle');
         startButton = document.getElementById('start');
         text = document.getElementById("tuto");
+        buttonSound = document.getElementById('my-audio');
+        ambientSound = document.getElementById("ambient-audio");
+        score = document.getElementById("score")
     }
 
 })(window, document, undefined);
 
+function playMusic() {
+    buttonSound.play();
+}
 function startGame() {
+
     if (startButton.textContent === "start") {
+
         isPlaying = true;
+        timer();
         element.style.display = 'none';
         startButton.style.display = 'none';
+        ambientSound.play()
     } if (startButton.textContent === "restart") {
+
         location.reload();
     }
 
@@ -28,7 +40,23 @@ function startGame() {
 function reload() {
     element.style.display = '';
     startButton.style.display = '';
+    score.style.display = 'block';
+    score.textContent += sec - 1
     element.textContent = "You loose"
     element.setAttribute('data-text', 'Sorry');
     startButton.textContent = "restart"
+    clearInterval(Scoretimer)
+}
+function timer() {
+
+
+    Scoretimer = setInterval(function () {
+
+        document.getElementById('changeTime').innerHTML = '' + sec;
+        sec++;
+
+
+    }, 1000);
+
+
 }
